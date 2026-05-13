75cdb27936bbb20c87c7af7d6c4ff21ffaa6182f
Ali Nahib <ali.nahib23@gmail.com>
2026-05-13 22:41:06 +0200

    Adding source code

---
+++ annotation-publisher/src/main/java/com/loki/annotationpublisher/AnnotationPublisherApplication.java
@@ -0,0 +1,12 @@
+package com.loki.annotationpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationPublisherApplication.class, args);
+    }
+}
